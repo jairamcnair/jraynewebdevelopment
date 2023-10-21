0152880c876667e034f8f6c67355db69ce1f7188
Jaira McNair <jaimcnair10@gmail.com>
2023-10-21 10:44:19 -0400

    fixed o in letterarray

--- sarahapp/Keyboard3.js
+++ sarahapp/Keyboard3.js
@@ -33,7 +33,7 @@ function typing(clicked_id){
             const numberarray = [
                 "+", "-", "x", "%", "=", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "(", ")", "@", "^", "{", "}", "$", ":", ";", "#", "/", "~"
             ]
-            const letterarray = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "0", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "?"]
+            const letterarray = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "?"]
             //alert(id);
             if(value2 === 'a'){
                 for(let i = 0; i < 27; ++i){
